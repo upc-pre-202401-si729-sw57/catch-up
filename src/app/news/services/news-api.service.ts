0de2b851ff0a1c8b7a2a5773c210e38a672c9d4e
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-10 16:34:56 -0500

    feat(news-api-service-client): implemented news api service client.

---
+++ src/app/news/services/news-api.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+  apiKey = 'fecf4feeffa64e4da682e7d268612ce5';
+  baseUrl = 'https://newsapi.org/v2';
+
+  constructor(private http: HttpClient) { }
+
+  // Fetch Sources information
+  getSources() {
+    return this.http.get(`${this.baseUrl}/top-headlines/sources?apiKey=${this.apiKey}`);
+  }
+
+  // Fetch Articles for given source id
+  getArticlesBySourceId(sourceId: string) {
+    return this.http.get(`${this.baseUrl}/top-headlines?sources=${sourceId}&apiKey=${this.apiKey}`);
+  }
+
+  initArticles() {
+    return this.getArticlesBySourceId('bbc-news');
+  }
+
+
+
+}
